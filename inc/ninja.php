6efc5fe2f1a7d7ee3b0257dcf68cd6fff73c8af0
Anna Borja <kingcity4@gmail.com>
2012-05-24 21:26:15 -0500

    Add files left behind in last commit

--- inc/ninja.php
+++ inc/ninja.php
@@ -1,54 +1,57 @@
 <div class="row-fluid">
     <div class="span1">&nbsp;</div>
     <div class="span10">
-        <div class="hero-unit footer">
+        <div class="hero-unit last">
             <h1>Ninjabook</h1>
             <form action="/index.php?p=ninja" method="post">
+                <label for="books">Enter a list of books&#8212;<em>one book per line, please!</em></label>
                 <div class="row-fluid">
-                    <label for="books"><p>Enter a list of books&#8212;<em>one
-                        book per line, please!</em></p></label>
-                </div>
-                <div class="row-fluid">
-                    <textarea class="span10" rows="15" id="books" name="books"></textarea>
+                    <div class="span10">
+                        <textarea class="span12" rows="15" id="books" name="books">The Ninja Handbook
+REAL Ultimate Power
+I'm a Ninja, You're a Ninja
+NinjaNinjaNinja
+                        </textarea>
+                    </div>
                     <div class="span1">
-                        <button class="btn btn-danger btn-large" type="submit" 
-                            name="submit">始めましょう!</button>
+                        <button class="btn btn-danger btn-large" type="submit" name="submit">始めましょう !</button>
                     </div>
                 </div>
-                <div class="row-fluid">
-                    <div class="span11">&nbsp;</div>
-                </div>
             </form>
-<tt>
-<?php
+<code><?php
 if (isset($_POST['submit']))
 {
-    $books = explode("\n", $_POST['books']);
-    $title_lengths = array_map('strlen', $books);
+    $untrimmed_titles = explode("\n", $_POST['books']);
+    $titles = array();
+
+    foreach ($untrimmed_titles as $untrimmed_title)
+        $titles[] = trim($untrimmed_title);
+
+    $title_lengths = array_map('strlen', $titles);
     $max_title_length = max($title_lengths);
-    $horiz = array();
-    sort($books);
+    $letter_rows = array();
+
+    sort($titles);
 
     for ($i = 0; $i < $max_title_length; ++$i)
-        $horiz[$i] = '&nbsp;';
+        $letter_rows[$i] = '&nbsp;';
 
-    foreach ($books as $book)
+    foreach ($titles as $title)
     {
-        $book = trim($book);
-        $book_title_length = strlen($book);
+        $title_length = strlen($title);
 
-        for ($i = 0; $i < $book_title_length; ++$i)
-            $horiz[$i] .= $book[$i].'&nbsp; &nbsp;';
+        for ($i = 0; $i < $title_length; ++$i)
+            $letter_rows[$i] .= $title[$i].'&nbsp;&nbsp;&nbsp;';
 
-        for ($i; $i < $max_title_length; ++$i)
-            $horiz[$i] .= '&nbsp; &nbsp; ';
+        for ( ; $i < $max_title_length; ++$i)
+            $letter_rows[$i] .= '&nbsp;&nbsp;&nbsp;&nbsp;';
     }
 
-    foreach ($horiz as $h)
-        echo $h, '<br>';
+    foreach ($letter_rows as $letter_row)
+        echo $letter_row, '<br>';
 }
-?>
-</tt>
+?></code>
         </div>
     </div>
 </div>
+
--- inc/zombie.php
+++ inc/zombie.php
@@ -1,21 +1,22 @@
 <div class="row-fluid">
     <div class="span1">&nbsp;</div>
     <div class="span10">
-        <div class="hero-unit">
-            <h1>Zappos Zombie Zurvival</h1>
-            <h2>Your survival guide for the zombie apocalypse</h2>
+        <header><div class="hero-unit">
+            <hgroup>
+                <h1>Zappos Zombie Zurvival</h1>
+                <h2>Your survival guide for the zombie apocalypse</h2>
+            </hgroup>
             <div class="row-fluid">
-                <img class="span4" src="/assets/img/saturn.jpg"
-                    alt="'Saturn Devouring His Son' by Francisco Goya">
+                <img class="span4" src="/img/saturn.jpg" alt="'Saturn Devouring His Son' by Francisco Goya">
                 <div class="span8">
-                    <p>Lorem ipsum placeholder text! Lorem ipsum placeholder text!
-                        Lorem ipsum placeholder text! Lorem ipsum placeholder text!
-                        Good luck!</p>
-                    <p class="text-center">
-                        <a class="btn btn-danger btn-large" href="#1">Begin >></a></p>
+                    <p>Lorem ipsum placeholder text! Lorem ipsum placeholder text! Lorem ipsum placeholder text!
+                        Lorem ipsum placeholder text!</p>
+                    <p>Lorem ipsum placeholder text! Lorem ipsum placeholder text! Lorem ipsum placeholder text!
+                        Lorem ipsum placeholder text!</p>
+                    <p class="text-center"><a class="btn btn-danger btn-large" href="#1">Tip #1 >></a></p>
                 </div>
             </div>
-        </div>
+        </div></header>
     </div>
 </div>
 <div class="row-fluid">
@@ -23,16 +24,18 @@
         <section id="1"><div class="hero-unit">
             <h1 class="text-center">Stock Up on Supplies</h1>
             <div class="row-fluid">
-                <img class="span12" src="/assets/img/supplies.jpg" alt="Canned food"
+                <img class="span12" src="/img/supplies.jpg" alt="Canned food"
                     title="Original photo by English Wikipedia user Daniel Case
 [GFDL or CC-BY-SA-3.0], via Wikimedia Commons">
             </div>
             <p>Lorem ipsum placeholder text! Lorem ipsum placeholder text!
                 Lorem ipsum placeholder text! Lorem ipsum placeholder text!
                 Good luck!</p>
-            <p class="text-center">
-                <a class="btn btn-danger btn-large" href="#top">Top ^</a>&nbsp;
-                <a class="btn btn-danger btn-large" href="#2">Next >></a></p>
+            <p>Lorem ipsum placeholder text! Lorem ipsum placeholder text!
+                Lorem ipsum placeholder text! Lorem ipsum placeholder text!
+                Good luck!</p>
+            <p class="text-center"><a class="btn btn-danger btn-large" href="#top">Top ^</a>&nbsp;
+                <a class="btn btn-danger btn-large" href="#2">Tip #2 >></a></p>
         </div></section>
     </div>
 </div>
@@ -42,8 +45,7 @@
         <section id="2"><div class="hero-unit">
             <h1 class="text-center">Aim for the Brain</h1>
             <div class="row-fluid">
-                <img class="span12" src="/assets/img/brain.jpg" alt="A human brain"
-                    title="Original photo by John A Beal,
+                <img class="span12" src="/img/brain.jpg" alt="A human brain" title="Original photo by John A Beal,
 PhD Dep't. of Cellular Biology & Anatomy,
 Louisiana State University
 Health Sciences Center Shreveport
@@ -52,10 +54,12 @@ Health Sciences Center Shreveport
             <p>Lorem ipsum placeholder text! Lorem ipsum placeholder text!
                 Lorem ipsum placeholder text! Lorem ipsum placeholder text!
                 Good luck!</p>
-            <p class="text-center">
-                <a class="btn btn-danger btn-large" href="#1"><< Previous</a>&nbsp;
+            <p>Lorem ipsum placeholder text! Lorem ipsum placeholder text!
+                Lorem ipsum placeholder text! Lorem ipsum placeholder text!
+                Good luck!</p>
+            <p class="text-center"><a class="btn btn-danger btn-large" href="#1"><< Tip #1</a>&nbsp;
                 <a class="btn btn-danger btn-large" href="#top">Top ^</a>&nbsp;
-                <a class="btn btn-danger btn-large" href="#3">Next >></a></p>
+                <a class="btn btn-danger btn-large" href="#3">Tip #3 >></a></p>
         </div></section>
     </div>
 </div>
@@ -64,7 +68,7 @@ Health Sciences Center Shreveport
         <section id="3"><div class="hero-unit">
             <h1 class="text-center">When in Doubt, RUN</h1>
             <div class="row-fluid">
-                <img class="span12" src="/assets/img/run.jpg" alt="Someone running"
+                <img class="span12" src="/img/run.jpg" alt="Someone running"
                     title="Original photo by Mike Baird from Morro Bay, USA
 (jogger-girl-surf-morro-bay_0880)
 [CC-BY-2.0], via Wikimedia Commons">
@@ -72,28 +76,28 @@ Health Sciences Center Shreveport
             <p>Lorem ipsum placeholder text! Lorem ipsum placeholder text!
                 Lorem ipsum placeholder text! Lorem ipsum placeholder text!
                 Good luck!</p>
-            <p class="text-center">
-                <a class="btn btn-danger btn-large" href="#2"><< Previous</a>&nbsp;
-                <a class="btn btn-danger btn-large" href="#top">Top ^</a>&nbsp;
-                <a class="btn btn-danger btn-large" href="#4">Next >></a></p>
+            <p>Lorem ipsum placeholder text! Lorem ipsum placeholder text!
+                Lorem ipsum placeholder text! Lorem ipsum placeholder text!
+                Good luck!</p>
+            <p class="text-center"><a class="btn btn-danger btn-large" href="#2"><< Tip #2</a>&nbsp;
+                <a class="btn btn-danger btn-large" href="#top">Top ^</a>&nbsp; <a class="btn btn-danger btn-large"
+                href="#4">End >></a></p>
         </div></section>
     </div>
 </div>
 <div class="row-fluid">
     <div class="span1">&nbsp;</div>
     <div class="span10">
-        <section id="4"><div class="hero-unit footer">
+        <section id="4"><div class="hero-unit last">
             <h1 class="text-center">End</h1>
             <p>Well, that's it! Good luck!</p>
             <div class="row-fluid">
-                <img class="span12" src="/assets/img/sunrise.jpg" alt="Sunrise"
-                    title="Original photo by Simon Eugster
+                <img class="span12" src="/img/sunrise.jpg" alt="Sunrise" title="Original photo by Simon Eugster
 --Simon 14:29, 5 July 2006 (UTC)
 (Own work) [GFDL, CC-BY-SA-3.0,
 or CC-BY-SA-2.5-2.0-1.0], via Wikimedia Commons">
             </div>
-            <p class="text-center">
-                <a class="btn btn-danger btn-large" href="#3"><< Previous</a>&nbsp;
+            <p class="text-center"><a class="btn btn-danger btn-large" href="#3"><< Tip #3</a>&nbsp;
                 <a class="btn btn-danger btn-large" href="#top">Top ^</a></p>
         </div></section>
     </div>
